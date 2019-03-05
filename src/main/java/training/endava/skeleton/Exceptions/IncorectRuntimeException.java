4de706f587e28bb3a48e03776a3ea3be4ef2a0cc
irimiacosmin <irimia.cosmin@gmail.com>
2019-03-05 12:36:50 +0200

    Fixed bugs 1 Aici abcd explic ce am adaugat in ; [Task: 1]

---
+++ src/main/java/training/endava/skeleton/Exceptions/IncorectRuntimeException.java
@@ -0,0 +1,7 @@
+package training.endava.skeleton.Exceptions;
+
+public class IncorectRuntimeException extends RuntimeException {
+    public IncorectRuntimeException(String errorMessage) {
+        super(errorMessage);
+    }
+}
