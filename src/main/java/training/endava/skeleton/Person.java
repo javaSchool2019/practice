6c7e42d3ef6c50923dedecc216b64968ecc33fdd
DavidMarcu <david.marcu.1997@gmail.com>
2019-03-06 16:02:57 +0200

    Java Basics1 Added Person model, PersonRepository and custom exception [Task: git practice]

---
+++ src/main/java/training/endava/skeleton/Person.java
@@ -0,0 +1,56 @@
+package training.endava.skeleton;
+
+import java.util.UUID;
+
+public class Person implements SkeletonObject<Person, UUID> {
+    private UUID id;
+    private String name;
+    private String phoneNumber;
+    private String address;
+
+    public Person(UUID id, String name, String phoneNumber, String address){
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    @Override
+    public UUID getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(UUID i) {
+        id = i;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString(){
+        return "ID: " + id + ", name: " + name + ", phoneNumber: " + phoneNumber + ", address: " + address;
+    }
+}
