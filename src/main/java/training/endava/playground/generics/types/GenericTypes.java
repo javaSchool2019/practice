6533e70fa0e10b1d3d7688d770d838cb4dbdf5d7
anduralex <alex.andur@yahoo.com>
2019-03-06 16:36:42 +0200

    Updated Collections&Generics Multi-line description of commit, feel free to be detailed. [Task: X]

--- src/main/java/training/endava/playground/generics/types/GenericTypes.java
+++ src/main/java/training/endava/playground/generics/types/GenericTypes.java
@@ -1,4 +1,6 @@
 package training.endava.playground.generics.types;
+import java.sql.SQLOutput;
+import java.util.*;
 
 public class GenericTypes {
 
@@ -14,6 +16,55 @@ public class GenericTypes {
         Box<Fruit> box2 = new Box<>();
         box2.packBox(new Fruit(300));
 
+        List<String> tags1 = new ArrayList<String>();
+        tags1.add("Food");
+        tags1.add("Health");
+        tags1.add("Vegan");
+
+        List<String> tags2 = new ArrayList<String>();
+        tags2.add("Cars");
+        tags2.add("Auto");
+        tags2.add("Vehicle");
+
+        List<String> tags3 = new ArrayList<String>();
+        tags3.add("Cars");
+        tags3.add("Health");
+
+        Article articol0 = new Article(tags1, "Vegans", "Iulia");
+        Article articol1 = new Article(tags2, "Automobilism", "David");
+        Article articol2 = new Article(tags3, "AutoAndFood", "Daea");
+        List<Article> listOfArts = new ArrayList<Article>();
+        listOfArts.add(articol0);
+        listOfArts.add(articol1);
+        listOfArts.add(articol2);
+        System.out.println(listOfArts);
+        //sort
+        listOfArts.sort(new Comparator<Article>() {
+            @Override
+            public int compare(Article o1, Article o2) {
+                return o1.getTitle().compareTo(o2.getTitle());
+            }
+        });
+        System.out.println(listOfArts );
+
+        //unique tags
+        Set<String> tagUnique = new HashSet<String>();
+        for(Article allTags: listOfArts)
+            tagUnique.addAll(allTags.getTags());
+        System.out.println(tagUnique);
+
+        //group by number of tags
+        HashMap<Integer, List<Article>> MapOfTags = new HashMap<Integer, List<Article>>();
+        for(Article arts: listOfArts)
+        {    int totalTags = arts.getTags().size();
+            if(MapOfTags.containsKey(totalTags))
+                MapOfTags.get(totalTags).add(arts);
+            else
+                MapOfTags.put(totalTags,new ArrayList<>());
+                MapOfTags.get(totalTags).add(arts);
+        }
+        MapOfTags.forEach((k,v) -> System.out.println("Key = "
+                + k + ", Value = " + v));
 
     }
 
---
+++ src/main/java/training/endava/skeleton/db/ExceptiaA.java
@@ -0,0 +1,8 @@
+package training.endava.skeleton.db;
+
+public class ExceptiaA extends RuntimeException{
+    public ExceptiaA(String param)
+    {
+        super(param);
+    }
+}
